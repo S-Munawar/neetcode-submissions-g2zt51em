ad8bafa08e6d347a1332bd0e1045fe301e086922
Shaik Abdul Munawar <shaikmunawar907@gmail.com>
2026-04-15 08:03:37 +0530

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+
+        function palindrome(l, r) {
+            while(l < r) {
+                if(s[l] !== s[r]) return false
+                l++
+                r--
+            }
+            return true
+        }
+
+        let n = s.length
+        let l = 0
+        let r = n - 1
+        
+        while(l < r) {
+            if(s[l] !== s[r]) {
+                 return (palindrome(l, r - 1) || palindrome(l + 1, r))
+            }
+            l++
+            r--
+        }
+        return true
+    }
+}
