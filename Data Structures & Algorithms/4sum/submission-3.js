299d0beb5d41b5579eb2c178aea758b706bef2c9
Shaik Abdul Munawar <shaikmunawar907@gmail.com>
2026-04-20 09:49:36 +0530

    Add: 4sum - submission-3

---
+++ Data Structures & Algorithms/4sum/submission-3.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[][]}
+     */
+    fourSum(nums, target) {
+
+        let res = []
+        let n = nums.length
+
+        nums.sort((a, b) => a - b)
+
+        for(let i = 0; i < n - 3; i++) {
+
+            if(i > 0 && nums[i] === nums[i - 1]) continue
+
+            for(let j = i + 1; j < n - 2; j++) {
+
+                if(j > i + 1 && nums[j] === nums[j - 1]) continue
+
+                let l = j + 1
+                let r = n - 1
+
+                while(l < r) {
+
+                    let sum = nums[i] + nums[j] + nums[l] + nums[r]
+                    if(sum === target) {
+                        res.push([nums[i], nums[j], nums[l], nums[r]])
+                        l++
+                        r--
+                        while(l < r && nums[l] === nums[l - 1]) l++
+                        while(l < r && nums[r] === nums[r + 1]) r++
+                    }
+                    else if(sum > target) {
+                        r--
+                    }
+                    else{
+                        l++
+                    }
+                }
+
+            }
+
+        }
+        return res
+    }
+}
