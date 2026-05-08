cd5bfc07925cc85786acdfc4fc5e0c470e48098e
Shaik Abdul Munawar <shaikmunawar907@gmail.com>
2026-05-08 15:32:05 +0530

    Add: string-encode-and-decode - submission-21

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-21.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = ""
+        for(const str of strs) {
+            result += str.length + '#' + str
+        }
+        return result
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const result = []
+        let j = 0
+
+        while(j < str.length) {
+            let i = j
+            while(str[j] !== '#') {
+                j++
+            }
+            let sz = parseInt(str.slice(i, j))
+            i = j + 1
+            j = i + sz
+            result.push(str.slice(i, j))
+        }
+        return result
+    }
+}
